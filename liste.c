c2156e97f6fff3ee17f4881787944a0c2feda172
Thibault Santonja <thibault.santonja@gmail.com>
2017-11-16 19:16:39 +0100

    taff au pic

--- liste.c
+++ liste.c
@@ -47,6 +47,9 @@ int insert_after_position (List *list, char *str, int p) {
 	int cmpt = 0;
 
 	while (cmpt < p) {
+		if (next_elem == NULL)
+			return -1;
+
 		next_elem = next_elem->next;
 		cmpt++;
 	}
@@ -54,29 +57,66 @@ int insert_after_position (List *list, char *str, int p) {
 	new_elem->data = str;
 	new_elem->next = next_elem->next;
 	next_elem->next = new_elem; 
+
+	return 0;
 }
 
 
 int remove (List *list, int p) {
+	Elem* next_elem = list->head;
+	int cmpt = 0;
 
+	while (cmpt < p) {
+		if (next_elem == NULL)
+			return -1;
+		
+		next_elem = next_elem->next;
+		cmpt++;
+	}
+
+	next_elem->next = next_elem->next->next;
+	free (next_elem->next->next);
+
+	return 0;
 }
 
 
 int compare (char *str1, char *str2) {
+	int i = 1;
+	int res = 0;
+	unsigned int mult = 1;
 
+	for (; i <= MAX; i++, mult *= 10) 
+		res = (str1 [MAX - i] - str2 [MAX - i]) * mult
+
+	if (res > 0)
+		return 1;
+	return 2;
 }
 
 
 int sort (List *list) {
-
+	/* TODO */
 }
 
 
 void display (List *list) {
-
+	/* TODO */
 }
 
 
 void destruct (List *list) {
+	Elem* next_elem = list->head;
+	Elem* elem_destroy;
 
+	while (next_elem != NULL) {
+		free (next_elem->data);
+		elem_destroy = next_elem;
+		next_elem = next_elem->next;
+		free (elem_destroy);
+	}
+
+	free (list->head);
+	free (list->tail);
+	free (list);
 }
